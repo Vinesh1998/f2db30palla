255e53c3dd45bd0af03f803994baddcea5c33612
Vinesh1998 <111940054+Vinesh1998@users.noreply.github.com>
2022-11-30 17:08:15 -0600

    added all the files

--- routes/juice.js
+++ routes/juice.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const juice_controllers= require('../controllers/juice');
+// A little function to check if we have an authorized user and continue on 
+
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
-router.get('/',juice_controllers.juice_view_all_Page);
+router.get('/',secured,juice_controllers.juice_view_all_Page);
 
 
 /* GET detail juice page */ 
-router.get('/detail', juice_controllers.juice_view_one_Page); 
+router.get('/detail',secured, juice_controllers.juice_view_one_Page); 
 
 /* GET create juice page */ 
-router.get('/create', juice_controllers.juice_create_Page);
+router.get('/create',secured, juice_controllers.juice_create_Page);
 
 /* GET create update page */ 
-router.get('/update', juice_controllers.juice_update_Page); 
+router.get('/update',secured, juice_controllers.juice_update_Page); 
 
 /* GET delete juice page */ 
-router.get('/delete', juice_controllers.juice_delete_Page); 
+router.get('/delete',secured, juice_controllers.juice_delete_Page); 
 
 
 module.exports = router;
